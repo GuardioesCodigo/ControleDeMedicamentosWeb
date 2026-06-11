fedfdbe1cf8b30dd784909a0c94694be4666ea2c
Dayuã-Santana <daniponsantana@gmail.com>
2026-06-11 20:31:15 -0300

    fix: Adicionando Scripts no _Layout.cshtml

--- ControleDeMedicamentos.WebApp/ModuloPaciente/Apresentacao/PacientesViewModel.cs
+++ ControleDeMedicamentos.WebApp/ModuloPaciente/Apresentacao/PacientesViewModel.cs
@@ -38,4 +38,7 @@ public class ExcluirPacienteViewModel
     public Guid Id { get; set; }
     public string Nome { get; set; } = string.Empty;
     public string CartaoSus { get; set; } = string.Empty;
+    public string CPF { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+
 }
